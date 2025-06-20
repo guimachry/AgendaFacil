b6ca902127bd8d71464a48399f53a710d37e7911
guimachry <geomachry@gmail.com>
2025-06-20 15:20:50 -0300

    correcao das funções e uso de struct para otimização

--- src.cpp
+++ src.cpp
@@ -1,53 +1,62 @@
 #include <stdio.h>
 
-void CadastrarPessoas(){
+
+
+typedef struct{
+	int quantidade_pessoas;
+	int quantidade_horarios;
+	char pessoas[100][100];
+	int horarios[20];		
+}Dados;
+
+void CadastrarPessoas(Dados *dados){
 	
 	// menu de cadastro de pessoas 
 	printf("-------------------\n");
 	printf("CADASTRO DE PESSOAS\n");
 	printf("-------------------\n");
 	
-	int quantidade_pessoas;
+	
 	printf("informe quantas pessoas irao partiipar da reuniao ? : ");
-	scanf("%d", &quantidade_pessoas);
+	scanf("%d", &dados->quantidade_pessoas);
 	
 	// aqui é onde vai criar um vetor pra armazenar as strings
 	// o primeiro indice é o tamanho
 	// e o segundo o tamanho máximno da string
-	char pessoas[quantidade_pessoas][100];
 	
 	
-	for(int i = 0; i < quantidade_pessoas; i++){
+	
+	for(int i = 0; i < dados->quantidade_pessoas; i++){
 		printf("informe nome %d :  ", i +1);
-		scanf("%s", pessoas[i]);
+		scanf("%s", dados->pessoas[i]);
 	}
 	
-	printf("%d participantes : \n", quantidade_pessoas);
-	for(int i = 0; i < quantidade_pessoas; i++){
-		printf("%d - %s \n", i + 1, pessoas[i]);
+	printf(" participantes : \n");
+	for(int i = 0; i < dados->quantidade_pessoas; i++){
+		printf("%d - %s \n", i + 1, dados->pessoas[i]);
 	}
 	
 	
 }
 
-void cadastrarHorarios() {
+void cadastrarHorarios(Dados *dados) {
 	
 	printf("-------------------\n");
 	printf("CADASTRO DE HORARIOS\n");
 	printf("-------------------\n");
 	
-    int qtd, i = 0, entrada = 1, j, igual;
+    int  i = 0, entrada = 1, j, igual;
     
     // aqui ele vai fazer a coleta da quantidade de horarios pra registro
     printf("Quantos horarios deseja adicionar ? ");
-    scanf("%d", &qtd);
-    int horarios[qtd];
+    scanf("%d", &dados->quantidade_horarios);
+    
     
 	// este laço vai verificar se o "i" é menor que a quantidade e se a entrada é diferente de 0
 	// sao 2 condiçoes pra ele funcionar
 	// ai ele faz as verificações e cadastra
 
-    while (i < qtd && entrada!= 0) {
+    while (i < dados->quantidade_horarios && entrada!= 0) {
     	// definido intervalo fixo de horarios pra cadastro, ou seja, so entre as 8 e as 18 pra evitar erros.
         printf("Digite horarios entra as 8 e as 18 / 0 encerra: ");
         scanf("%d", &entrada);
@@ -65,7 +74,7 @@ void cadastrarHorarios() {
         igual = 0;
         // esse laço percorre o vetor pra ver se a entrada ja tem um horario ocupado, se tem ela joga 1 pra igual
         for (j = 0; j < i; j++) {
-            if (horarios[j] == entrada) {
+            if (dados->horarios[j] == entrada) {
 				igual = 1;
         	}
 		}
@@ -74,7 +83,7 @@ void cadastrarHorarios() {
         if (igual == 1) {
             printf("horario ocupado \n");
         } else {
-            horarios[i] = entrada;
+            dados->horarios[i] = entrada;
             i++;
             printf("Horario registrado com sucesso!\n");
         }
@@ -82,15 +91,30 @@ void cadastrarHorarios() {
 	
 	// print dos horaiors cadastrados
     printf("Horários Cadastrados: ");
-    for (j = 0; j < i; j++) {
+    for (j = 0; j < dados->quantidade_horarios; j++) {
     	
-        	printf("%d ", horarios[j]);
+        	printf("%d ", dados->horarios[j]);
     }
     
     printf("\n");
 }
-void registrarDisponibilidade(){
+
+
+
+void registrarDisponibilidade(Dados *dados, int disponibilidade[100][20]){
 	
+	printf("-------------------\n");
+	printf("REGISTRO Disponibilidade\n");
+	printf("-------------------\n");
+	
+	for(int i = 0; i < dados->quantidade_pessoas; i++){
+		printf("olá %s, informe disponibilidade, para SIM 1/ NAO 0. \n", dados->pessoas[i]);
+		for(int j = 0; j < dados->quantidade_horarios; j++){
+			printf("Disponivel as %d horas ? ",dados->horarios[j]);
+			scanf("%d", &disponibilidade[i][j]);
+		}
+	}
+		
 }
 
 void processarDisponibilidade(){
@@ -106,8 +130,12 @@ void exibirRelatorioCompleto(){
 
 int main(){
 	
-	CadastrarPessoas();
-	cadastrarHorarios();
+	Dados dados;
+	int disponibilidade[100][20];
+	
+	CadastrarPessoas(&dados);
+	cadastrarHorarios(&dados);
+	registrarDisponibilidade(&dados, disponibilidade);
 	
 	
 }
